12d9ed52ca4d6d0e88bd19f06c2c86901a480694
Martin Cyster <martincyster@fastmail.com>
2021-05-11 15:00:05 +1000

    Using shapely to define a polygon for randomly generating new atomic positions

--- src/structure.py
+++
@@ -1,4 +0,0 @@
-def get_surface_height(substrate, coordinates, percentage_of_box_to_search=80.0):
-    cutoff = substrate["c"] * (percentage_of_box_to_search / 100)
-    z = [xyz[2] for xyz in coordinates if xyz[2] < cutoff]
-    return max(z)
