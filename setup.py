074114769a6c6a2d3972f189a0b90e39de3d6a5e
Martin Cyster <martincyster@fastmail.com>
2021-11-29 11:24:46 +1100

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -15,5 +15,16 @@ setup(
     author_email='martincyster@fastmail.com',
     url='https://github.com/mmtn/deposition',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        "click==7.1.2",
+        "numpy==1.20.2",
+        "pandas==1.2.4",
+        "pymatgen==2022.0.8",
+        "PyYAML==5.4.1",
+        "schema==0.7.4",
+        "Shapely==1.7.1",
+        "Sphinx==4.2.0",
+    ]
 )
+
